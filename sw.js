4e6dee54dbf10c79753d825e482f16c8cd92daf6
kometh-hash <kometh@rcc.ac.th>
2026-03-11 10:38:11 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,11 @@
-const CACHE_NAME = 'stock-flow-v1';
+const CACHE_NAME = 'stock-flow-v2'; // อัปเดตเป็น v2 เพื่อล้างแคชเก่า
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['./', './index.html'])));
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll([
+        './', 
+        './index.html', 
+        './manifest.json', 
+        './309723.png'
+    ])));
 });
 self.addEventListener('fetch', (e) => {
     e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
